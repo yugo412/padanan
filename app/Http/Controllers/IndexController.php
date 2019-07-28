6bc789bf3a0a64be4acc7717ea8d10c05be494e2
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2019-07-28 23:39:35 +0700

    Cache index data for faster load

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Word;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\View\View;
 
 class IndexController extends Controller
@@ -15,12 +16,18 @@ class IndexController extends Controller
      */
     public function __invoke(Request $request): View
     {
-        $categories = Category::orderBy('name')
-            ->withCount('words')
-            ->get();
+        $categories = Cache::remember('category.index', now()->addDay(), function (){
+            return Category::orderBy('name')
+                ->withCount('words')
+                ->get();
+        });
 
         $number = new \NumberFormatter('id_ID', \NumberFormatter::DECIMAL);
-        $wordCount = $number->format(Word::count());
+        $wordCount = $number->format(
+            Cache::remember('word.count', now()->addMinutes(30), function (){
+                return Word::count();
+            })
+        );
 
         return \view('index', compact('categories', 'wordCount', 'number'))
             ->with('title', __('Cari :count padanan kata asing dalam bahasa Indonesia', ['count' => $wordCount]));
