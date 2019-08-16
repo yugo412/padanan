0a249a337e54f6cecae989626b01f26e8b481d4f
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2019-08-16 14:05:34 +0700

    Fix typo and add new template line

--- app/Console/Commands/Twitter/PostWordCommand.php
+++ app/Console/Commands/Twitter/PostWordCommand.php
@@ -51,6 +51,8 @@ class PostWordCommand extends Command
             ];
 
             $templates = [
+                __('Apakah kamu tahu padanan dari istilah :origin? Jawabannya adalah :locale (:category). #padanan #glosarium', $replaces),
+                __('Istilah :origin dikenal sebagai :locale dalam bahasa Indonesia. #padanan #glosarium', $replaces),
                 __('Padanan :origin dalam :category adalah :locale. #padanan #glosarium', $replaces),
                 __('Padanan dalam bidang :category:line:origin = :locale:line#padanan #glosarium', $replaces),
                 __('Dalam bahasa Indonesia, :origin berarti :locale. #padanan #glosarium', $replaces),
@@ -58,7 +60,7 @@ class PostWordCommand extends Command
                 __('Apa itu :origin?:lineDalam bahasa Indonesia dikenal sebagai :locale (:category).:line#padanan #glosarium', $replaces),
                 __(':origin = :locale (:category) #padanan #glosarium', $replaces),
                 __('Dalam bahasa Indonesia, istilah :origin dipadankan sebagai :locale (:category). #padanan #glosarium', $replaces),
-                __('Kalau dalam bahasa asing disebut :origin, maka dalam basaha Indonesia disebut :locale. Istilah ini umum ada pada bidang :category. #padanan #glosarium', $replaces),
+                __('Kalau dalam bahasa asing disebut :origin, maka dalam bahasa Indonesia disebut :locale. Istilah ini umum ada pada bidang :category. #padanan #glosarium', $replaces),
             ];
 
             Twitter::send(collect($templates)->random());
