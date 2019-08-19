8a3b711686890af9deb0205325b433793837e13d
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2019-08-19 16:47:21 +0700

    Add tweet button

--- resources/views/word/content.blade.php
+++ resources/views/word/content.blade.php
@@ -14,7 +14,19 @@
     <h2>{{ $word->locale }}</h2>
     <hr>
 
+    @php
+      $tweet = __('Padanan istilah :origin (:category) adalah :locale.:line:link', [
+        'origin' => $word->origin,
+        'locale' => $word->locale,
+        'category' => strtolower($word->category->name),
+        'line' => str_repeat(PHP_EOL, 2),
+        'link' => route('word.show', $word),
+      ]);
+    @endphp
+
     <div class="mt-4">
+      <a href="https://twitter.com/intent/tweet?hashtags=padanan,glosarium&text={{ urlencode($tweet) }}"
+         class="btn btn-outline-info btn-sm text-info"><i class="fab fa-twitter"></i> @lang('Bagikan')</a>
       <loveable link="{{ route('word.love', $word) }}" count="{{ $word->total_likes }}"></loveable>
       <reportable link="{{ route('word.report', $word) }}" total="{{ $word->reports_count }}" auth="{{ auth()->check() }}"></reportable>
     </div>
