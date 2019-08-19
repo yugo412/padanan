d3ddfff470b9acfcc70e829a51c532b8dd632f98
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2019-08-20 00:22:45 +0700

    Add via username to Twitter share button

--- resources/views/word/content.blade.php
+++ resources/views/word/content.blade.php
@@ -15,19 +15,19 @@
     <hr>
 
     @php
-      $tweet = __(':twitter Padanan istilah :origin (:category) adalah :locale.:line:link', [
+      $tweet = __('Padanan istilah :origin (:category) adalah :locale.:line:link', [
         'origin' => $word->origin,
         'locale' => $word->locale,
         'category' => strtolower($word->category->name),
         'line' => str_repeat(PHP_EOL, 2),
         'link' => route('word.show', $word),
-        'twitter' => config('twitter.username'),
       ]);
     @endphp
 
     <div class="mt-4">
-      <a href="https://twitter.com/intent/tweet?hashtags=padanan,glosarium&text={{ urlencode($tweet) }}"
-         class="btn btn-outline-info btn-sm text-info" target="_blank"><i class="fab fa-twitter"></i> @lang('Bagikan')
+      <a
+        href="https://twitter.com/intent/tweet?via={{ str_replace('@', '', config('twitter.username')) }}&hashtags=padanan,glosarium&text={{ urlencode($tweet) }}"
+        class="btn btn-outline-info btn-sm text-info" target="_blank"><i class="fab fa-twitter"></i> @lang('Bagikan')
       </a>
       <loveable link="{{ route('word.love', $word) }}" count="{{ $word->total_likes }}"></loveable>
       <reportable link="{{ route('word.report', $word) }}" total="{{ $word->reports_count }}" auth="{{ auth()->check() }}"></reportable>
--- resources/views/word/show.blade.php
+++ resources/views/word/show.blade.php
@@ -36,18 +36,18 @@
               <p>@lang('Tidak ada deskripsi untuk padanan kata ini.')</p>
 
               @php
-                $tweet = __(':twitter Padanan istilah :origin (:category) adalah :locale.:line:link', [
+                $tweet = __('Padanan istilah :origin (:category) adalah :locale.:line:link', [
                   'origin' => $word->origin,
                   'locale' => $word->locale,
                   'category' => strtolower($word->category->name),
                   'line' => str_repeat(PHP_EOL, 2),
                   'link' => route('word.show', $word),
-                  'twitter' => config('twitter.username'),
                 ]);
               @endphp
 
-              <a href="https://twitter.com/intent/tweet?hashtags=padanan,glosarium&text={{ urlencode($tweet) }}"
-                 class="btn btn-outline-info btn-sm text-info" target="_blank"><i
+              <a
+                href="https://twitter.com/intent/tweet?via={{ str_replace('@', '', config('twitter.username')) }}&hashtags=padanan,glosarium&text={{ urlencode($tweet) }}"
+                class="btn btn-outline-info btn-sm text-info" target="_blank"><i
                   class="fab fa-twitter"></i> @lang('Bagikan')</a>
               <loveable link="{{ route('word.love', $word) }}" count="{{ $word->total_likes }}"></loveable>
               <reportable link="{{ route('word.report', $word) }}" total="{{ $word->reports_count }}"
