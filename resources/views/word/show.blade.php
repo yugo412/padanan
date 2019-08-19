2b58abefda3d4acf79bb60247dbc00dc5ff20fd8
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2019-08-19 17:02:34 +0700

    Add tweet button and Disqus comments

--- resources/views/word/show.blade.php
+++ resources/views/word/show.blade.php
@@ -8,6 +8,10 @@
 
 @section('content')
   <div class="container">
+    <section class="adsense">
+      @include('layouts.adsense.responsive')
+    </section>
+
     <div id="doc-header" class="doc-header text-center"></div><!--//doc-header-->
 
     <div class="doc-body row">
@@ -23,14 +27,26 @@
             <div class="section-block">
 
               <div class="jumbotron text-left">
-                  <h2>{{ $word->origin }}</h2>
-                  <hr>
-                  <h2>{{ $word->locale }}</h2>
+                <h2>{{ $word->origin }}</h2>
+                <hr>
+                <h2>{{ $word->locale }}</h2>
               </div>
 
               <h3>@lang('Deskripsi')</h3>
               <p>@lang('Tidak ada deskripsi untuk padanan kata ini.')</p>
 
+              @php
+                $tweet = __('Padanan istilah :origin (:category) adalah :locale.:line:link', [
+                  'origin' => $word->origin,
+                  'locale' => $word->locale,
+                  'category' => strtolower($word->category->name),
+                  'line' => str_repeat(PHP_EOL, 2),
+                  'link' => route('word.show', $word),
+                ]);
+              @endphp
+
+              <a href="https://twitter.com/intent/tweet?hashtags=padanan,glosarium&text={{ urlencode($tweet) }}"
+                 class="btn btn-outline-info btn-sm text-info"><i class="fab fa-twitter"></i> @lang('Bagikan')</a>
               <loveable link="{{ route('word.love', $word) }}" count="{{ $word->total_likes }}"></loveable>
               <reportable link="{{ route('word.report', $word) }}" total="{{ $word->reports_count }}"
                           auth="{{ auth()->check() }}"></reportable>
@@ -38,9 +54,10 @@
 
           </section><!--//doc-section-->
 
-          <section class="adsense">
-            @include('layouts.adsense.responsive')
-          </section>
+          @include('layouts.partials.disqus', [
+            'url' => url()->current(),
+            'slug' => $word->slug,
+          ])
 
         </div><!--//content-inner-->
       </div><!--//doc-content-->
