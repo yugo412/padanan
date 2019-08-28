6282638c0d1cfdc8fa7243f03fe892b1c6f65b71
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2019-08-28 18:02:35 +0700

    Fix typo and add new lines for bot

--- app/Console/Commands/Twitter/PostWordCommand.php
+++ app/Console/Commands/Twitter/PostWordCommand.php
@@ -57,14 +57,17 @@ class PostWordCommand extends Command
             ];
 
             $templates = [
+                __('Mungkin, sebagian dari kita pernah mendengar istilah :origin, tapi belum tahu padanannya. Istilah ini punya padanan :locale. #padanan #glosarium', $replaces),
+                __('Apa padanan dari :origin? :locale jawabannya. #padanan #glosarium', $replaces),
+                __('Belum tahu padanan dari :origin? Ini padanannya: :locale (:category). #padanan #glosarium', $replaces),
                 __('Apakah kamu tahu padanan dari istilah :origin? Jawabannya adalah :locale (:category). #padanan #glosarium', $replaces),
                 __('Istilah :origin dikenal sebagai :locale dalam bahasa Indonesia. #padanan #glosarium', $replaces),
                 __('Padanan :origin dalam :category adalah :locale. #padanan #glosarium', $replaces),
-                __('Padanan dalam bidang :category:line:origin = :locale:line#padanan #glosarium', $replaces),
+                __('Padanan dalam bidang :category:line:origin = :locale.:line#padanan #glosarium', $replaces),
                 __('Dalam bahasa Indonesia, :origin berarti :locale. #padanan #glosarium', $replaces),
-                __('Tahukan kamu kalo padanan dari istilah :origin dalam bidang :category adalah :locale? #padanan #glosarium', $replaces),
+                __('Tahukah kamu kalo padanan dari istilah :origin dalam bidang :category adalah :locale? #padanan #glosarium', $replaces),
                 __('Apa itu :origin?:lineDalam bahasa Indonesia dikenal sebagai :locale (:category).:line#padanan #glosarium', $replaces),
-                __(':origin = :locale (:category) #padanan #glosarium', $replaces),
+                __(':origin = :locale (:category). #padanan #glosarium', $replaces),
                 __('Dalam bahasa Indonesia, istilah :origin dipadankan sebagai :locale (:category). #padanan #glosarium', $replaces),
                 __('Kalau dalam bahasa asing disebut :origin, maka dalam bahasa Indonesia disebut :locale. Istilah ini umum ada pada bidang :category. #padanan #glosarium', $replaces),
             ];
