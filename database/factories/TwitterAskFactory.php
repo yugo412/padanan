e5b1d1afff93a38e5b9f93ae4a0443706b011e8e
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2019-09-01 23:39:47 +0700

    Auto-reply question from Twitter

---
+++ database/factories/TwitterAskFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\TwitterAsk;
+use Faker\Generator as Faker;
+
+$factory->define(TwitterAsk::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
